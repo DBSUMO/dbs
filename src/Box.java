b1b3fb8ae4dd2b4c3f6a42a7bbb826c4093085c5
Administrator <Administrator@chenthilraj>
2016-03-12 17:45:41 +0530

    Box Changed

--- src/Box.java
+++ src/Box.java
@@ -12,12 +12,14 @@ public class Box {
 		return h*w;
 	}
 	
+	
 	public static void main(String[] args) {
 		Box b = new Box();
 		int x = b.volume();
 		System.out.println("The Volume of the box"+x);
 		int x1=b.area();
 		System.out.println("The area of the box"+x1);
+		System.out.println("Added for GitHub");
 
 	}
 
